19b5f792a48baf3b5c5271205c02e52973ce6d03
sohukia <sohukia@gmail.com>
2026-06-27 11:20:10 +0200

    feat: data downloadable and cleaned up

---
+++ draft.py
@@ -0,0 +1,22 @@
+"""
+code_commune,
+nom_commune,
+code_departement,
+code_postal,
+id_parcelle,
+id_adresse,
+adresse_,
+id_mutation,
+annee,
+date_mutation,
+nature_mutation,
+valeur_fonciere,
+id_local,
+type_local,
+surface_reelle_bati,
+nombre_pieces_principales,
+"""
+
+"""
+Identifiant de document|Reference document|1 Articles CGI|2 Articles CGI|3 Articles CGI|4 Articles CGI|5 Articles CGI|No disposition|Date mutation|Nature mutation|Valeur fonciere|No voie|B/T/Q|Type de voie|Code voie|Voie|Code postal|Commune|Code departement|Code commune|Prefixe de section|Section|No plan|No Volume|1er lot|Surface Carrez du 1er lot|2eme lot|Surface Carrez du 2eme lot|3eme lot|Surface Carrez du 3eme lot|4eme lot|Surface Carrez du 4eme lot|5eme lot|Surface Carrez du 5eme lot|Nombre de lots|Code type local|Type local|Identifiant local|Surface reelle bati|Nombre pieces principales|Nature culture|Nature culture speciale|Surface terrain
+"""
